cf5d670ff115b9f45067e50dfa48389b67a3d751
poonnu <poojanrathod@gmail.com>
2016-06-29 07:00:17 +0530

    Initial commit

---
+++ core/src/com/teampyroxinc/volt/Volt.java
@@ -0,0 +1,48 @@
+package com.teampyroxinc.volt;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Volt extends Game {
+	public static final int WIDTH = 480;
+	public static final int HEIGHT = 800;
+
+	public static final String TITLE = "Bolt";
+	private GameStateManager gsm;
+
+	private Music music;
+
+	private SpriteBatch batch;
+
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		gsm = new GameStateManager();
+		music = (Gdx.audio.newMusic(Gdx.files.internal("music.mp3")));
+		music.setLooping(true);
+		music.setVolume(0.5f);
+		music.play();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gsm.push(new MenuState(gsm));
+	}
+
+	@Override
+	public void render () {
+
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		music.dispose();
+	}
+}
